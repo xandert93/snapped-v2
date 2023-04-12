2e01b987fb214d302d939cfbfdc7e27abc1fa117
Alex Tharmaratnam <xandert.93@outlook.com>
2023-04-12 11:12:22 +0100

    client/disable Redux DevTools for production

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -2,9 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducer from './root-reducer';
 import { socketMiddleware, httpMessageMiddleware } from './middleware';
 
+const inProduction = process.env === 'production';
+
 export const store = configureStore({
   reducer,
   middleware: (gDM) => [...gDM(), socketMiddleware, httpMessageMiddleware],
+  devTools: !inProduction,
 });
 
 // store.subscribe(() => console.log("Redux state updated!"))
